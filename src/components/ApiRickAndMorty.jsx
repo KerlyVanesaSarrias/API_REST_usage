6a87659cf6559227adda6ac44893e291f3ddabec
Kerly Sarrias <kerlysarrias011@gmail.com>
2024-10-02 11:00:14 -0500

    change styles cards

--- src/components/ApiRickAndMorty.jsx
+++ src/components/ApiRickAndMorty.jsx
@@ -22,10 +22,10 @@ export const ApiRickAndMorty = () => {
     return (
         <div>
             <h2 className='text-center text-white display-3 mb-4'>Rick and Morty</h2>
-            <div className='mx-3 d-flex flex-wrap row-cols-4 row-cols-md-4 row-cols-lg-4 overflow-auto vh-80 scrollable-container'>
+            <div className='mx-3 d-flex flex-wrap row-cols-4 row-cols-md-4 row-cols-lg-5 overflow-auto vh-80 scrollable-container'>
                 {characters.map((characters, index) => (
                     <div key={index}>
-                        <div className='cards my-2 ; '>
+                        <div className='cards my-3 ; '>
                             <img src={characters.image} className='fixed-img p-3 ' alt="Rick and Morty" />
                             <section className="card_body">
                                 <h3 className="card_name">{characters.name}</h3>
